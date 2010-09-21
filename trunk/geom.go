ac8b589bf631b31e619427230841b874f93901fa
azserrata <azserrata@e628e9bc-2d80-ac80-c65b-9284e30e8080>
2010-09-21 19:43:42 +0000

    Centroid

--- trunk/geom.go
+++ trunk/geom.go
@@ -1,6 +1,7 @@
-/* David Fernandes
+/* =====================
+      David Fernandes
    daugfernandes@aim.com
-	====================== */
+   ===================== */
 
 package geom
 
@@ -9,13 +10,42 @@ import (
 	"math"
 )
 
+// Point2D
 type Point2D struct {
 	X float64
 	Y float64
 }
 
+// Calc distance between 2 points
+func (p1 *Point2D) Distance(p2 *Point2D) float64 {
+	return math.Sqrt(math.Pow(p1.X-p2.X, 2) + math.Pow(p1.Y-p2.Y, 2))
+}
+
+func (p *Point2D) Centroid() *Point2D {
+	return &Point2D{p.X,p.Y}
+}
+
+// Rotate point
+func (p *Point2D) Rotate(center *Point2D, angle float64) *Point2D {
+	d := p.Distance(center)
+	return &Point2D{center.X + d*math.Cos(angle),
+		center.Y + d*math.Sin(angle)}
+}
+
+func (p *Point2D) String() string {
+	return fmt.Sprintf("Point2d->%s%f%s%f", "X:", p.X, ",Y:", p.Y)
+}
+
+/*===========================================================================
+  Polyline2D
+  ===========================================================================*/
+
 type Polyline2D struct {
 	Vertexes []Point2D
+}
+
+func (pl *Polyline2D) String() string {
+	return fmt.Sprintf("Polyline2D->%d%s",len(pl.Vertexes)," points")
 }
 
 // Calc length of polyline
@@ -34,22 +64,20 @@ func (pl *Polyline2D) Length() float64 {
 	return result
 }
 
-// Calc distance between 2 points
-func (p1 *Point2D) Distance(p2 *Point2D) float64 {
-	return math.Sqrt(math.Pow(p1.X-p2.X, 2) + math.Pow(p1.Y-p2.Y, 2))
-}
-
-// Rotate point
-func (p1 *Point2D) Rotate(center *Point2D, angle float64) *Point2D {
-	d := p1.Distance(center)
-	return &Point2D{center.X + d*math.Cos(angle),
-		center.Y + d*math.Sin(angle)}
-}
-
-func (p1 *Point2D) String() string {
-	return fmt.Sprintf("%s%f%s%f", "X:", p1.X, ",Y:", p1.Y)
-}
-
-func (pl *Polyline2D) String() string {
-	return "x"
+// Calc Centroid of polyline
+func (pl *Polyline2D) Centroid() *Point2D {
+	var tx, ty, tl, l float64 = 0, 0, 0, 0
+	var pPrev Point2D
+	for i, vtx := range pl.Vertexes {
+		if i == 0 {
+			pPrev = vtx
+		} else {
+			l = vtx.Distance(&pPrev)
+			tl += l 
+			tx += (vtx.X + pPrev.X) * (l / 2)
+			ty += (vtx.Y + pPrev.Y) * (l / 2)
+		}
+	}
+	fmt.Println(tx/(l),ty/(l))
+	return &Point2D{tx / tl, ty / tl}
 }
