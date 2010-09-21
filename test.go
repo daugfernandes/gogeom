9214073282aafb38889c6e732b1557ef9b6bcdb6
azserrata <azserrata@e628e9bc-2d80-ac80-c65b-9284e30e8080>
2010-09-21 19:43:42 +0000

    Centroid

--- test.go
+++ test.go
@@ -7,17 +7,17 @@ import (
 
 func main() {
 
-	p1 := &geom.Point2D{10,0}
-	p2 := &geom.Point2D{20,10}
-	p3 := &geom.Point2D{10,20}
-	p4 := &geom.Point2D{0,10}
-	p5 := &geom.Point2D{10,0}
+	p1 := &geom.Point2D{10,10}
+	p2 := &geom.Point2D{10,20}
+	p3 := &geom.Point2D{20,20}
 
-  	pa := []geom.Point2D{*p1, *p2, *p3, *p4, *p5}
+  	pa := []geom.Point2D{*p1, *p2, *p3}
 
 	ps1 := &geom.Polyline2D{pa};
 
-	fmt.Println(ps1.Length())
+	fmt.Println(ps1.Centroid().String())
+	//fmt.Println(ps1.Length(),ps1.String())
+	//fmt.Println("++",p1.Centroid().String())
 
 	return
 }
