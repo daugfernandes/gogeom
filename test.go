0540310acf9842aab5fb3693abb26cf7f471e1dc
azserrata@gmail.com <azserrata@gmail.com@e628e9bc-2d80-ac80-c65b-9284e30e8080>
2010-09-21 23:04:59 +0000

    License information

--- test.go
+++ test.go
@@ -1,3 +1,28 @@
+/*
+ *
+ *     Copyright (C) 2010  David Fernandes
+ *
+ *                         Rua da Quinta Amarela, 60
+ *                         4475-663 MAIA
+ *                         PORTUGAL
+ *
+ *                         <daugfernandes@aim.com>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package main
 
 import (
@@ -7,14 +32,15 @@ import (
 
 func main() {
 
-	p1 := &geom.Point2D{10,10}
-	p2 := &geom.Point2D{10,20}
-	p3 := &geom.Point2D{20,20}
-	p4 := &geom.Point2D{20,10}
+	p1 := &geom.Point2D{10,0}
+	p2 := &geom.Point2D{20,10}
+	p3 := &geom.Point2D{10,20}
+	p4 := &geom.Point2D{0,10}
+
 	
 	ps1 := &geom.Polygon2D{[]geom.Point2D{*p1, *p2, *p3, *p4, *p1}};
 
-	fmt.Println(ps1.Area())
+	fmt.Println(ps1.MBR().Area())
 
 
 	return
