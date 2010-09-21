f3ab9578aa3425c76b2970148abbafb42f3e7520
azserrata@gmail.com <azserrata@gmail.com@e628e9bc-2d80-ac80-c65b-9284e30e8080>
2010-09-21 22:36:17 +0000

    Polygon2D and some improvments

--- test.go
+++ test.go
@@ -8,16 +8,14 @@ import (
 func main() {
 
 	p1 := &geom.Point2D{10,10}
-	p2 := &geom.Point2D{20,20}
-	p3 := &geom.Point2D{30,10}
+	p2 := &geom.Point2D{10,20}
+	p3 := &geom.Point2D{20,20}
+	p4 := &geom.Point2D{20,10}
+	
+	ps1 := &geom.Polygon2D{[]geom.Point2D{*p1, *p2, *p3, *p4, *p1}};
 
-  	pa := []geom.Point2D{*p1, *p2, *p3}
+	fmt.Println(ps1.Area())
 
-	ps1 := &geom.Polyline2D{pa};
-
-	fmt.Println(ps1.Centroid().String())
-	//fmt.Println(ps1.Length(),ps1.String())
-	//fmt.Println("++",p1.Centroid().String())
 
 	return
 }
